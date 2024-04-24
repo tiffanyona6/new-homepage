02b05299d8a700ce4a7011d23a77543b30b6274a
tiffanyona6 <tiffanyona6@gmail.com>
2024-04-24 21:55:04 +0200

    actualizado 24.04

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
+
+const buttonElement = document.getElementById("icon-menu");
+const menuElement = document.getElementById("menu");
+
+const handleMenu = () => {
+  if (menuElement.classList.contains("menu--show")) {
+    menuElement.classList.remove("menu--show");
+  } else {
+    menuElement.classList.add("menu--show");
+  }
+};
+buttonElement.addEventListener("click", handleMenu);
